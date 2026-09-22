9b03e2a0c5cb68e3e350cf3a06f0dcf141ba540c
hathwik06 <vtu30092@veltech.edu.in>
2026-09-22 12:18:18 +0530

    Time: 77 ms (22.55%), Space: 106.5 MB (76.19%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> s = new Stack<>();
+        int[] ans = new int[temperatures.length]; 
+        for (int i = temperatures.length - 1; i >= 0; i--) {
+            while (!s.isEmpty() && temperatures[i] >= temperatures[s.peek()]) {
+                s.pop();
+            }
+            
+            if (s.isEmpty()) {
+                ans[i] = 0;
+            } else {
+                ans[i] = s.peek() - i;
+            }
+            
+            s.push(i);
+        }
+        return ans;
+    }
+}
