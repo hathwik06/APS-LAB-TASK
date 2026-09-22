31f3ebfe7c027ffd589c1a55cc6c712cbbeced10
hathwik06 <vtu30092@veltech.edu.in>
2026-09-22 12:17:38 +0530

    Time: 33 ms (36.14%), Space: 55.5 MB (24.19%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+class StockSpanner {
+    Stack<int[]> s;
+
+    public StockSpanner() {
+        s = new Stack<>();
+    }
+
+    public int next(int price) {
+        int span = 1;
+        while (!s.isEmpty() && price >= s.peek()[0]) {
+            span = span + s.pop()[1];  
+        }
+        s.push(new int[]{price, span});
+        
+        return span;
+    }
+}
